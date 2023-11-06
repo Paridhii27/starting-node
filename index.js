0cb418e610f08e855978a9fc15d5ed42f9b20751
Paridhi <paridhigarg27@gmail.com>
2023-11-06 18:11:23 -0500

    Express file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hi!")
+// Import the express library and assign it to a variable
+import express from 'express'
+
+// Create an instance of an express application 
+const app = express()
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001 
+
+// Set up a response for the root path of the application
+// two parameters: 1. route (particular location or file path is found) 2. runs parameter req,res at that location
+// what do you want to do when you get a specific request so response is defined
+// '/' is to refer to homepage
+app.get('/', (req, res) => {
+  res.send("Write an instruction for a drawing here. For example: draw a line across the page, draw 10 circles of different sizes, draw a duck.")
+})
+
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}` )
+})
